9a067a01c52264beec7204b23df15d89014409cc
Hassan Mir <hassanmir92@gmail.com>
2017-05-23 21:47:10 +0100

    Ensuring State and Country are saved on companies

--- app/assets/javascripts/google_places_autocomplete.js
+++ app/assets/javascripts/google_places_autocomplete.js
@@ -7,5 +7,12 @@ function initAutocomplete() {
   var input = (document.getElementsByClassName('google-places-autocomplete')[0])
   if (input) {
     autocomplete = new google.maps.places.Autocomplete(input, options);
+
+    autocomplete.addListener('place_changed', function() {
+      var place = autocomplete.getPlace();
+
+      $('.location-state').val(place.address_components[0].long_name);
+      $('.location-country').val(place.address_components[place.address_components.length - 1].long_name);
+    });
   }
 }
