cea7183a60141b65b46009569c8686625b975f44
Hassan Mir <hassanmir92@gmail.com>
2017-06-05 21:39:06 +0100

    Publication validation update

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -5,6 +5,11 @@ class Publication < ActiveRecord::Base
   enum participation: %w(editor writer)
   enum category: %w(book catalogue article essay review paper)
 
-  validates :category, :title, :publication_title, :day, :month, :year, presence: true
-  validates :participation, presence: true, if: -> { user.role == "professional" }
+  validates :category, :title, :publication_title, :day, :month, :year, presence: true, unless: :attachment_is_present?
+  validates :participation, presence: true, if: -> { user.role == "professional" && !attachment_is_present? }
+
+  def attachment_is_present?
+    !pdf_url.blank?
+  end
+
 end
