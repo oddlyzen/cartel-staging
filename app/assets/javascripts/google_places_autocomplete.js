a5bdf9f2e664dfbb3a7b900375a95fd33d25f5b8
Hassan Mir <hassanmir92@gmail.com>
2017-07-11 21:52:22 +0100

    Location autocomplete fix for multiple objects

--- app/assets/javascripts/google_places_autocomplete.js
+++ app/assets/javascripts/google_places_autocomplete.js
@@ -18,88 +18,94 @@ function initAutocomplete() {
 }
 
 function initAutocompleteExhibition() {
-  var input = (document.getElementsByClassName('google-places-autocomplete-exhibition')[0])
-  if (input) {
-    autocompleteExhibition = new google.maps.places.Autocomplete(input, options);
+  $('.google-places-autocomplete-exhibition').each(function(index, input) {
+    if (input) {
+      autocompleteExhibition = new google.maps.places.Autocomplete(input, options);
 
-    autocompleteExhibition.addListener('place_changed', function() {
+      autocompleteExhibition.addListener('place_changed', function() {
 
-      var place = autocompleteExhibition.getPlace();
+        var place = autocompleteExhibition.getPlace();
 
-      $('.location-state-exhibition').val(place.address_components[0].long_name);
-      $('.location-country-exhibition').val(place.address_components[place.address_components.length - 1].long_name);
-    });
-  }
+        $(input).parent().siblings('.location-state-exhibition').val(place.address_components[0].long_name);
+        $(input).parent().siblings('.location-country-exhibition').val(place.address_components[place.address_components.length - 1].long_name);
+      });
+    }
+  });
 }
 
 function initAutocompleteExperience() {
-  var input = (document.getElementsByClassName('google-places-autocomplete-experience')[0])
-  if (input) {
-    autocompleteExperience = new google.maps.places.Autocomplete(input, options);
+  $('.google-places-autocomplete-experience').each(function(index, input) {
+    if (input) {
+      autocompleteExperience = new google.maps.places.Autocomplete(input, options);
 
-    autocompleteExperience.addListener('place_changed', function() {
-      var place = autocompleteExperience.getPlace();
+      autocompleteExperience.addListener('place_changed', function() {
+        var place = autocompleteExperience.getPlace();
 
-      $('.location-state-experience').val(place.address_components[0].long_name);
-      $('.location-country-experience').val(place.address_components[place.address_components.length - 1].long_name);
-    });
-  }
+        $(input).parent().siblings('.location-state-experience').val(place.address_components[0].long_name);
+        $(input).parent().siblings('.location-country-experience').val(place.address_components[place.address_components.length - 1].long_name);
+      });
+    }
+  });
 }
 
 function initAutocompleteAward() {
-  var input = (document.getElementsByClassName('google-places-autocomplete-award')[0])
-  if (input) {
-    autocompleteAward = new google.maps.places.Autocomplete(input, options);
+  $('.google-places-autocomplete-award').each(function(index, input) {
+    if (input) {
+      autocompleteAward = new google.maps.places.Autocomplete(input, options);
 
-    autocompleteAward.addListener('place_changed', function() {
-      var place = autocompleteAward.getPlace();
+      autocompleteAward.addListener('place_changed', function() {
+        var place = autocompleteAward.getPlace();
 
-      $('.location-state-award').val(place.address_components[0].long_name);
-      $('.location-country-award').val(place.address_components[place.address_components.length - 1].long_name);
-    });
-  }
+        $(input).parent().siblings('.location-state-award').val(place.address_components[0].long_name);
+        $(input).parent().siblings('.location-country-award').val(place.address_components[place.address_components.length - 1].long_name);
+      });
+    }
+  });
 }
 
 function initAutocompleteEducation() {
-  var input = (document.getElementsByClassName('google-places-autocomplete-education')[0])
-  if (input) {
-    autocompleteEducation = new google.maps.places.Autocomplete(input, options);
+  $('.google-places-autocomplete-education').each(function(index, input) {
+    if (input) {
+      autocompleteEducation = new google.maps.places.Autocomplete(input, options);
 
-    autocompleteEducation.addListener('place_changed', function() {
-      var place = autocompleteEducation.getPlace();
+      autocompleteEducation.addListener('place_changed', function() {
+        var place = autocompleteEducation.getPlace();
 
-      $('.location-state-education').val(place.address_components[0].long_name);
-      $('.location-country-education').val(place.address_components[place.address_components.length - 1].long_name);
-    });
-  }
+        $(input).parent().siblings('.location-state-education').val(place.address_components[0].long_name);
+        $(input).parent().siblings('.location-country-education').val(place.address_components[place.address_components.length - 1].long_name);
+      });
+    }
+  })
 }
 
 function initAutocompleteResidency() {
-  var input = (document.getElementsByClassName('google-places-autocomplete-residency')[0])
-  if (input) {
-    autocompleteResidency = new google.maps.places.Autocomplete(input, options);
+  $('.google-places-autocomplete-residency').each(function(index, input) {
+    if (input) {
+      autocompleteResidency = new google.maps.places.Autocomplete(input, options);
 
-    autocompleteResidency.addListener('place_changed', function() {
-      var place = autocompleteResidency.getPlace();
+      autocompleteResidency.addListener('place_changed', function() {
+        var place = autocompleteResidency.getPlace();
 
-      $('.location-state-residency').val(place.address_components[0].long_name);
-      $('.location-country-residency').val(place.address_components[place.address_components.length - 1].long_name);
-    });
-  }
+        $(input).parent().siblings('.location-state-residency').val(place.address_components[0].long_name);
+        $(input).parent().siblings('.location-country-residency').val(place.address_components[place.address_components.length - 1].long_name);
+      });
+    }
+  })
 }
 
 function initAutocompletePublication() {
-  var input = (document.getElementsByClassName('google-places-autocomplete-publication')[0])
-  if (input) {
-    autocompletePublication = new google.maps.places.Autocomplete(input, options);
+  $('.google-places-autocomplete-publication').each(function(index, input) {
+    if (input) {
+      autocompletePublication = new google.maps.places.Autocomplete(input, options);
 
-    autocompletePublication.addListener('place_changed', function() {
-      var place = autocompletePublication.getPlace();
+      autocompletePublication.addListener('place_changed', function() {
+        var place = autocompletePublication.getPlace();
 
-      $('.location-state-publication').val(place.address_components[0].long_name);
-      $('.location-country-publication').val(place.address_components[place.address_components.length - 1].long_name);
-    });
-  }
+        $(input).parent().siblings('.location-state-publication').val(place.address_components[0].long_name);
+        $(input).parent().siblings('.location-country-publication').val(place.address_components[place.address_components.length - 1].long_name);
+      });
+    }
+  });
 }
 
 $(document).ready(function() {
@@ -110,4 +116,16 @@ $(document).ready(function() {
   initAutocompleteEducation();
   initAutocompleteResidency();
   initAutocompletePublication();
+
+  $('.add_fields').on("mouseup", function() {
+    setTimeout(function() {
+      initAutocomplete();
+      initAutocompleteExhibition();
+      initAutocompleteExperience();
+      initAutocompleteAward();
+      initAutocompleteEducation();
+      initAutocompleteResidency();
+      initAutocompletePublication();
+    }, 100);
+  });
 })
