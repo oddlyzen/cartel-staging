03e0fcbe4d6f6eb062d559cbe7ccc92a77a0db35
Hassan Mir <hassanmir92@gmail.com>
2017-10-17 18:33:54 +0100

    Reset Password Email fix

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.exceptions_app = self.routes
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
@@ -77,7 +78,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'pan-staging.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => ENV['HOST_URL'] }
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
