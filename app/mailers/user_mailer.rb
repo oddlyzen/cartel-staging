ede972cbbeee9d769a380d5c35fd3f5be4d27ad0
Hassan Mir <hassanmir92@gmail.com>
2017-10-17 18:47:54 +0100

    Hotfix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,6 +7,7 @@ class UserMailer < ApplicationMailer
 
   def reset_password_email(user)
     @user = User.find user.id
+    @url  = edit_password_reset_url(@user.reset_password_token)
     mail(to: user.email, subject: 'Reset password email')
   end
 end
